27e02d7c74c4a8e649ddcd8b6adf0ad378439489
LobitoM365 <almariojuancho@gmail.com>
2023-05-31 18:49:22 -0500

    probando modificaciones

--- js/navMiguel.js
+++ js/navMiguel.js
@@ -3,11 +3,34 @@ let itemSubmenu = document.querySelectorAll(".item-submenu");
 let chevron = document.querySelectorAll(".chevron");
 let navUl = document.getElementById("navUl");
 let menuHamburguer = document.getElementById("menuHamburguer");
+let divHamburguer = document.getElementById("divHamburguer");
+let navHorizontal = document.getElementById("navHorizontal");
+
+let widthNavUl = navUl.scrollWidth;
+navUl.style.width = "75px";
+divHamburguer.style.width = "75px";
+let heightdivHamburguer = divHamburguer.scrollHeight;
+
+navUl.style.marginTop = (heightdivHamburguer + 20) + "px";
 
 menuHamburguer.addEventListener("click", function () {
-    navUl.classList.toggle("transition-nav-ul")
+    navUl.classList.toggle("nav-desplegado")
+    divHamburguer.classList.toggle("nav-desplegado")
+    if(navUl.classList.contains("nav-desplegado")){
+        navUl.style.width =  widthNavUl + "px";
+        divHamburguer.style.width =  widthNavUl + "px";
+        navUl.classList.remove("transition-nav-ul")
+        divHamburguer.classList.remove("transition-nav-ul")
+    }else{
+        navUl.style.width = "75px";
+        divHamburguer.style.width = "75px";
+        navUl.classList.add("transition-nav-ul")
+        divHamburguer.classList.add("transition-nav-ul")
+    }
+
     for (let x = 0; x < desplegarSubmenu.length; x++) {
         let height = itemSubmenu[x].scrollHeight;
+        console.log(height)
         itemSubmenu[x].classList.remove("focus2")
         itemSubmenu[x].classList.remove("focus3")
         itemSubmenu[x].classList.remove("focus4")
@@ -15,13 +38,14 @@ menuHamburguer.addEventListener("click", function () {
         itemSubmenu[x].style.marginTop = "-" + height + "px";
         itemSubmenu[x].classList.remove("transition-submenu-bottom");
         chevron[x].classList.remove("transition-chevron");
+        itemSubmenu[x].style.opacity = 0;
+        
     }
 })
 
 for (let i = 0; i < desplegarSubmenu.length; i++) {
     let height = itemSubmenu[i].scrollHeight;
     itemSubmenu[i].style.marginTop = "-" + height + "px";
-    console.log(height)
     desplegarSubmenu[i].addEventListener("click", function () {
         
         for (let x = 0; x < desplegarSubmenu.length; x++) {
@@ -41,17 +65,25 @@ for (let i = 0; i < desplegarSubmenu.length; i++) {
                 }
             }
             if (i == x) {
+                let height = itemSubmenu[i].scrollHeight;
                 itemSubmenu[i].classList.toggle("transition-submenu-bottom");
                 if(itemSubmenu[i].classList.contains("transition-submenu-bottom")){
                     itemSubmenu[i].style.marginTop = 0;
                     itemSubmenu[i].classList.add("transition-submenu-top");
+                    itemSubmenu[i].style.opacity = 1;
+                   
                 }else{
                     itemSubmenu[i].style.marginTop = "-" + height + "px";
+                    itemSubmenu[i].style.opacity = 0;
                     itemSubmenu[i].classList.add("transition-submenu-top");
                 }
 
-
+                navUl.classList.add("nav-desplegado")
+                navUl.style.width =  widthNavUl + "px";
                 navUl.classList.remove("transition-nav-ul")
+                divHamburguer.classList.add("nav-desplegado")
+                divHamburguer.style.width =  widthNavUl + "px";
+                divHamburguer.classList.remove("transition-nav-ul")
                 chevron[i].classList.toggle("transition-chevron");
                 itemSubmenu[x].classList.toggle("focus2");
                 itemSubmenu[x].classList.add("focus3");
@@ -63,7 +95,7 @@ for (let i = 0; i < desplegarSubmenu.length; i++) {
                             itemSubmenu[u].classList.add("focus4")
                         } else {
                             itemSubmenu[u].classList.remove("focus3");
-                            ;
+
                         }
                     }
                 }
@@ -74,6 +106,8 @@ for (let i = 0; i < desplegarSubmenu.length; i++) {
 
     });
 }
+
+
 
 
 
