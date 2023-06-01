1ea074b3793406ef8071ee51cad21dcbbcb77b94
TatianaVargasD <tatianavd03@gmail.com>
2023-06-01 11:43:35 -0500

    nuevocambio

--- js/tatiana.js
+++ js/tatiana.js
@@ -12,3 +12,40 @@ botonVertical.addEventListener("click",function (){
     }
     
 })
+let subMenu=document.querySelectorAll(".item-menu")
+let children = document.querySelectorAll(".children")
+
+for (let index = 0; index < subMenu.length; index++) {
+    subMenu[index].addEventListener("click",function () {
+    /* alert(index) */
+    if (children[index].classList.contains("children-desp")) {
+        children[index].classList.remove("children-desp")
+       
+    }else{
+        if(index==1){
+            if(children[1].classList.contains("children-two")){
+                children[1].classList.remove("children-two")
+                 
+            }else{
+            children[1].classList.add("children-two")
+
+            }
+            
+        }
+        else if(index==2){
+            if(children[2].classList.contains("children-three")){
+                children[2].classList.remove("children-three")
+                 
+            }else{
+            children[2].classList.add("children-three")
+
+            }
+           
+        }else{
+            children[index].classList.add("children-desp")
+        }
+    }
+
+})
+    
+}
