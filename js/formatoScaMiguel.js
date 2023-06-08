ca8312310aeb4f11a3c6edbb17486cd0f7e55ded
LobitoM365 <almariojuancho@gmail.com>
2023-06-08 00:23:22 -0500

    probando modificaciones

--- js/formatoScaMiguel.js
+++ js/formatoScaMiguel.js
@@ -1,6 +1,20 @@
-let value = document.getElementById("value");
-let range = document.getElementById("range");
 
-range.oninput = function(){
-    value.innerHTML= range.value;
+let valueRangeIntensidad = document.querySelectorAll(".value-range-intensidad");
+let rangeIntensidad = document.querySelectorAll(".range-intensidad");
+let puntajeRange  = document.querySelectorAll(".puntaje-range ");
+let rangePuntaje  = document.querySelectorAll(".range-puntaje ");
+
+
+for(let RI = 0; RI < rangeIntensidad.length ; RI++){
+    rangeIntensidad[RI].addEventListener("input", function(){
+        valueRangeIntensidad[RI].innerHTML= rangeIntensidad[RI].value;
+    })
+}
+
+
+for(let x = 0; x < rangePuntaje.length; x++){
+    rangePuntaje[x].addEventListener("input", function(){
+        puntajeRange[x].innerHTML = rangePuntaje[x].value;
+        
+    })
 }
--- js/formulariosMiguel.js
+++ js/formulariosMiguel.js
@@ -2,9 +2,46 @@ let select = document.querySelectorAll(".select");
 let labelSelect = document.querySelectorAll(".labelSelect");
 let label = document.querySelectorAll(".label");
 let input = document.querySelectorAll(".input");
+let inputNumberDocumento = document.getElementById("inputNumberDocumento");
+let alertInputDocumento = document.getElementById("alertInputDocumento");
+let inputNumberTelefono = document.getElementById("inputNumberTelefono");
+let alertInputTelefono = document.getElementById("alertInputTelefono");
+let inputText = document.querySelectorAll(".inputText");
+let inputNumber = document.querySelectorAll(".inputNumber");
+let alertInputVacio = document.querySelectorAll(".alert-input-vacio");
+let alertSelectVacio = document.querySelectorAll(".alert-select-vacio");
 let enviar = document.getElementById("enviar");
 let formulario = document.getElementById("formulario");
 let iframeAlertModal = document.getElementById("iframeAlertModal");
+let inputCorreo = document.getElementById("inputCorreo");
+let labelTelefono = document.getElementById("labelTelefono");
+let labelDocumento = document.getElementById("labelDocumento");
+
+
+inputNumberTelefono.addEventListener("input", function(){
+    if(inputNumberTelefono.value.length = 10 ){
+        inputNumberTelefono.value = inputNumberTelefono.value.slice(0,10);
+    }
+})
+
+inputNumberDocumento.addEventListener("input", function(){
+    if(inputNumberDocumento.value.length = 10 ){
+        inputNumberDocumento.value = inputNumberDocumento.value.slice(0,10);
+    }
+})
+
+
+for (let n = 0; n < inputNumber.length; n++) {
+    inputNumber[n].addEventListener("input", function () {
+        inputNumber[n].value = inputNumber[n].value.replace(/\D/g, "");
+    })
+}
+
+for (let t = 0; t < inputText.length; t++) {
+    inputText[t].addEventListener("input", function () {
+        inputText[t].value = inputText[t].value.replace(/\d+/g, "");
+    })
+}
 
 for (let i = 0; i < select.length; i++) {
     select[i].addEventListener("click", function () {
@@ -16,17 +53,29 @@ for (let i = 0; i < select.length; i++) {
             labelSelect[i].style.color = "orange";
             select[i].classList.add("transition-color");
             labelSelect[i].classList.add("transition-color");
+            alertSelectVacio[i].style.visibility= "hidden";
+            alertSelectVacio[i].style.opacity= "0";
         }
+       
     })
 
 }
 for (let x = 0; x < input.length; x++) {
     input[x].addEventListener("input", function () {
-        if (input[x].value == "") {
+        input[x].value = input[x].value.trimStart();
+        if (input[x].value.trim() == "") {
             label[x].style.color = "";
         } else {
             label[x].style.color = "orange";
             label[x].classList.add("transition-color");
+            alertInputVacio[x].style.visibility= "hidden";
+            alertInputVacio[x].style.opacity= "0";
+        }
+        if(inputNumberTelefono.value.length < 10 ){
+            labelTelefono.style.color = "white";
+        }
+        if(inputNumberDocumento.value.length < 8 || inputNumberDocumento.value.length == 9){
+            labelDocumento.style.color = "white";
         }
     })
 }
@@ -37,11 +86,15 @@ enviar.addEventListener("click", function (event) {
     for (let s = 0; s < select.length; s++) {
         if (select[s].value == "") {
             selectSinValor = 1;
+            alertSelectVacio[s].style.opacity = "1";
+            alertSelectVacio[s].style.visibility = "visible";
         }
     }
     for (let u = 0; u < input.length; u++) {
-        if (input[u].value == "") {
+        if (input[u].value.trim() == "") {
             inputSinValor = 1;
+            alertInputVacio[u].style.opacity = "1";
+            alertInputVacio[u].style.visibility = "visible";
         }
     }
     if (selectSinValor == 1 || inputSinValor == 1) {
@@ -51,6 +104,14 @@ enviar.addEventListener("click", function (event) {
     } else {
         iframeAlertModal.style.display = "block";
         event.preventDefault();
+    }
+    if(inputNumberTelefono.value.length < 10 ){
+        alertInputTelefono.style.opacity = "1";
+        alertInputTelefono.style.visibility = "visible";
+    }
+    if(inputNumberDocumento.value.length < 8 || inputNumberDocumento.value.length == 9){
+        alertInputDocumento.style.opacity = "1";
+        alertInputDocumento.style.visibility = "visible";
     }
 })
 
@@ -69,11 +130,11 @@ window.addEventListener('load', function () {
             }
         }
         for (let u = 0; u < input.length; u++) {
-            if (input[u].value == "") {
+            if (input[u].value.trim() == "") {
                 inputSinValor = 1;
             }
         }
-        if (selectSinValor == 1 || inputSinValor == 1) {
+        if (selectSinValor == 1 || inputSinValor == 1 || inputNumberTelefono.value.length < 10 || inputNumberDocumento.value.length < 8 || inputNumberDocumento.value.length == 9 ) {
             mistake.style.display = "block";
             noMistake.style.display = "none";
         } else {
