f8c75f08c4c25c203dea1cc11b83e83c9c885797
jfabian04 <ramosgalindezjerlinsonfabian@gmail.com>
2023-06-10 23:03:37 -0500

    Warnign regiter ok

--- js/registrarAprendizFabian.js
+++ js/registrarAprendizFabian.js
@@ -4,36 +4,61 @@ let modalError = document.getElementById("modal-error");
 let btnRegister = document.getElementById("register");
 let btnOk = document.querySelectorAll(".btnOk");
 let input = document.querySelectorAll(".inp");
-let txtWarning =  document.querySelectorAll(".txt-warning");
-let inputSelect = document.getElementById("selectDni");
-let inputSelectCourse = document.getElementById("selectCourse");
+let txtWarning = document.querySelectorAll(".txt-warning");
+let inputNumber = document.querySelectorAll(".inp-number");
+let inputLetter = document.querySelectorAll(".inp-letter");
 let inputValidated = 0;
+
+function removeWarning() {
+    for (let index = 0; index < input.length; index++) {
+        input[index].addEventListener("input", function () {
+            txtWarning[index].classList.remove("txt-warning-visible")
+            input[index].classList.remove("input-border")
+        })
+    }
+}
+removeWarning();
 
 function inputValidate() {
     for (let i = 0; i < input.length; i++) {
         if (input[i].value.trim() === '') {
             inputValidated = 0;
+            txtWarning[i].classList.add("txt-warning-visible")
+            input[i].classList.add("input-border")
         } else {
             inputValidated = inputValidated + 1;
         }
     }
-    return inputValidated
 }
-function warningPosition(){
-    for (let y = 0; y < txtWarning.length; y++) {
-        console.log()
+
+function onlyNumbers() {
+    for (let a = 0; a < inputNumber.length; a++) {
+        let inputValue = inputNumber[a].value;
+        let valueReplaced = inputValue.replace(/\D/g, '');
+        inputNumber[a].value = valueReplaced;
     }
-    return y
+}
+
+function onlyLetters() {
+    for (let a = 0; a < inputLetter.length; a++) {
+        // let inputValue = ;
+        let valueReplaced = inputLetter[a].value.replace(/\d/g, '');
+        inputLetter[a].value = valueReplaced;
+        let space = inputLetter[a].value.replace("  "," ");
+        inputLetter[a].value = space;
+        let spaceStart = inputLetter[a].value.trimStart();
+        inputLetter[a].value = spaceStart;
+        // inputLetter[a].value.
+    }
 }
 btnRegister.addEventListener("click", function () {
     inputValidate()
-    if (inputValidated >= input.length && inputSelect.value != '' && inputSelectCourse.value != '') {
+    if (inputValidated >= input.length) {
         modal.classList.add("container-modal-visible");
         modalError.classList.remove("modal-register-visible");
         modalGood.classList.add("modal-register-visible");
         console.log("Good")
     } else {
-        // txtWarning[y].classList.add("txt-warning-visible")
         modal.classList.add("container-modal-visible");
         modalGood.classList.remove("modal-register-visible");
         modalError.classList.add("modal-register-visible");
